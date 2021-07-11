a70dee769ec062c372d0361da4b5c3a13da6b469
usermuser <lwrdrstv@gmail.com>
2021-07-11 22:20:27 +0500

    added constructor to PizzaList component

--- pizzaproject/front_pizzaproject/src/pizzerias/pizzeriaslist.js
+++ pizzaproject/front_pizzaproject/src/pizzerias/pizzeriaslist.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import PizzaDetail from "./pizzeriadetail";
 
 class PizzaList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pizzeriasData: [],
+            pizzeria: "",
+            showComponent: false,
+        };
+        this.getPizzaDetail = this.getPizzaDetail.bind(this);
+        this.showPizzeriaDetails = this.showPizzeriaDetails.bind(this);
+    }
 
     state = {
         pizzeriasData: []
+    }
+
+    getPizzaDetail(item) {
+        axios
+            .get('http://127.0.0.1:8000/'.concat(item.get_absolute_url))
+            .then((response) => {
+                this.setState({pizzeria: response.data})
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    showPizzeriaDetails(item) {
+        this.getPizzaDetail(item);
+        this.setState({showComponent: true});
     }
 
     componentDidMount() {
@@ -14,16 +41,22 @@ class PizzaList extends Component {
             })
             .catch(function (error) {
                 console.log(error);
-            })
+            });
     }
 
     render() {
         return (
             <div>
                 {this.state.pizzeriasData.map(item => {
-                    return <h3 key={item.id}> {item.pizzeria_name}, {item.city}</h3>
-                })
-                }
+                    return (
+                        <h3 key={item.id} onClick={() => this.showPizzeriaDetails(item)}>
+                            {item.pizzeria_name}, {item.city}
+                        </h3>
+                    );
+                })}
+                {this.state.showComponent ? (
+                    <PizzaDetail pizzariaDetail={this.state.pizzeria} />
+                    ) : null}
             </div>
         )
     }
